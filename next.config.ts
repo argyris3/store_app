22756480720cd7e19b6ad93bb83366767928bffc
argyris3 <78222306+argyris3@users.noreply.github.com>
2025-02-07 08:48:08 +0200

    third commit

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
-  // eslint: {
-  //   ignoreDuringBuilds: true,
-  // },
+
   experimental: {
     serverActions: {
       bodySizeLimit: '100MB',
